40f83564f8a8736a5bc5ae69c1e1efcf317e4f5c
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-06-03 20:49:56 +0400

    Abstractions Added

---
+++ SimpleBank/SimpleBank.Core/Repositories/Abstractions/IAccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using SimpleBank.Core.Models;
+
+namespace SimpleBank.Core.Repositories.Abstractions
+{
+    public interface IAccountRepository
+    {
+        public Account GetAccountById(int id);
+
+        public int SaveAccount(Account customer);
+
+        public bool UpdateAccount(Account customer);
+
+        public bool DeleteAccount(Account customer);
+    }
+}
---
+++ SimpleBank/SimpleBank.Core/Repositories/Abstractions/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using SimpleBank.Core.Models;
+
+namespace SimpleBank.Core.Repositories.Abstractions
+{
+    public interface ICustomerRepository
+    {
+        public Customer GetCustomerById(int id);
+
+        public int SaveCustomer(Customer customer);
+
+        public bool UpdateCustomer(Customer customer);
+
+        public bool DeleteCustomer(Customer customer);
+    }
+}
