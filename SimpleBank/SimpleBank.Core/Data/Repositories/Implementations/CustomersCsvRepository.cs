7b984c09a3ccbc864ab9e2e8cdd53efdcdccd073
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-06-04 18:37:23 +0400

    Customer and Opereration repositories Implemented

--- SimpleBank/SimpleBank.Core/Data/Repositories/Implementations/CustomersCsvRepository.cs
+++ SimpleBank/SimpleBank.Core/Data/Repositories/Implementations/CustomersCsvRepository.cs
@@ -1,8 +1,34 @@
-﻿using SimpleBank.Core.Data.Repositories.Abstractions;
+﻿using SimpleBank.Core.Data.FileAccess;
+using SimpleBank.Core.Data.Repositories.Abstractions;
+using SimpleBank.Core.Models;
 
 namespace SimpleBank.Core.Data.Repositories.Implementations
 {
-    public class CustomersCsvRepository : ICustomersRepository
+    public class CustomersCsvRepository : GenericCsvRepository<Customer, int>
     {
+        public CustomersCsvRepository(IDataReader<string> dataReader, IDataWriter<string> dataWriter) : base(dataReader, dataWriter)
+        {
+        }
+
+        protected override Customer ToObject(string s)
+        {
+            var data = s.Split(',');
+            var idx = 0;
+            return new Customer(int.Parse(data[idx++]))
+            {
+                Name = data[idx++],
+                IdentityNumber = data[idx++],
+                PhoneNumber = data[idx++],
+                Email = data[idx++],
+                Type = byte.Parse(data[idx])
+            };
+        }
+
+        protected override string ToCsv(Customer obj)
+        {
+            return $"{obj.Name},{obj.IdentityNumber},{obj.PhoneNumber},{obj.Email},{obj.Type}";
+        }
+
+        protected override int GenerateNextId(int lastId) => lastId + 1;
     }
 }
--- SimpleBank/SimpleBank.Core/Data/Repositories/Implementations/OperationsCsvRepository.cs
+++ SimpleBank/SimpleBank.Core/Data/Repositories/Implementations/OperationsCsvRepository.cs
@@ -1,8 +1,34 @@
-﻿using SimpleBank.Core.Data.Repositories.Abstractions;
+﻿using System;
+using SimpleBank.Core.Data.FileAccess;
+using SimpleBank.Core.Data.Repositories.Abstractions;
+using SimpleBank.Core.Models;
 
 namespace SimpleBank.Core.Data.Repositories.Implementations
 {
-    public class OperationsCsvRepository : IOperationsRepository
+    public class OperationsCsvRepository : GenericCsvRepository<Operation, long>
     {
+        public OperationsCsvRepository(IDataReader<string> dataReader, IDataWriter<string> dataWriter) : base(dataReader, dataWriter)
+        {
+        }
+
+        protected override Operation ToObject(string s)
+        {
+            var data = s.Split(',');
+            var idx = 0;
+            return new Operation(long.Parse(data[idx++]))
+            {
+                Type = byte.Parse(data[idx++]),
+                Currency = data[idx++],
+                Amount = decimal.Parse(data[idx++]),
+                AccountId = int.Parse(data[idx++]),
+                CustomerId = int.Parse(data[idx++]),
+                HappenedAt = DateTime.Parse(data[idx])
+            };
+        }
+
+        protected override string ToCsv(Operation obj) =>
+            $"{obj.Type},{obj.Currency},{obj.Amount},{obj.AccountId},{obj.CustomerId},{obj.HappenedAt}";
+
+        protected override long GenerateNextId(long lastId) => lastId + 1;
     }
 }
