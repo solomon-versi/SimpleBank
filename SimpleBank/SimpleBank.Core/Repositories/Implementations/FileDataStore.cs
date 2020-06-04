f7c55fc9702da59a5c559a04cbc384603b8b2045
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-06-04 18:06:11 +0400

    FileDataStore Added

---
+++ SimpleBank/SimpleBank.Core/Repositories/Implementations/FileDataStore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using SimpleBank.Core.Repositories.Abstractions;
+
+namespace SimpleBank.Core.Repositories.Implementations
+{
+    public class FileDataStore : IDataWriter<string>, IDataReader<string>
+    {
+        private readonly string _filePath;
+
+        public FileDataStore(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Write(IEnumerable<string> data)
+        {
+            File.WriteAllLines(_filePath, data);
+        }
+
+        public IEnumerable<string> Read()
+        {
+            return File.ReadAllLines(_filePath);
+        }
+    }
+}
