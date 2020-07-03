8a36f9b70053bd7d97ca59edd198db77793db2db
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-07-03 20:51:58 +0400

    Entity Framework project added

---
+++ SimpleBank/SimpleBank.Data/Repository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SimpleBank.Core.Data.Repositories.Abstractions;
+using SimpleBank.Core.Models;
+
+namespace SimpleBank.Data
+{
+    public class AccountsRepository : IRepository<Account, int>
+    {
+        public Account GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Add(Account entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Account entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Account> Query(Func<Account, bool> @where)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class OperationRepository : IRepository<Operation, long>
+    {
+        public Operation GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Add(Operation entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Operation entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Operation> Query(Func<Operation, bool> @where)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CustomerRepository : IRepository<Customer, int>
+    {
+        public Customer GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> Query(Func<Customer, bool> @where)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpleBank/SimpleBank.Data/SimpleBankDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SimpleBank.Data.Models;
+
+namespace SimpleBank.Data
+{
+    public class SimpleBankDbContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Operation> Operations { get; set; }
+        public DbSet<CurrencyRate> CurrencyRates { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDb; Initial Catalog = SimpleBank");
+        }
+    }
+}
