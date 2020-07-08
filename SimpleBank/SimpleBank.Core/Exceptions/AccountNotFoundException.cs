379cb206424631f2670eda28d80fa29591cef103
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-07-08 19:56:16 +0400

    Domain Exceptions modified

--- SimpleBank/SimpleBank.Core/Exceptions/AccountNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SimpleBank.Core.Exceptions
-{
-    public class AccountNotFoundException : DomainException
-    {
-        public AccountNotFoundException(in int accountId) : base($"Account with id: {accountId} not found")
-        {
-        }
-    }
-}
---
+++ SimpleBank/SimpleBank.Core/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleBank.Core.Exceptions
+{
+    public class NotFoundException<T> : DomainException
+    {
+        public NotFoundException(in long id) : base($"{typeof(T).Name} with id: {id} not found")
+        {
+        }
+
+        public NotFoundException(string message, System.Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
